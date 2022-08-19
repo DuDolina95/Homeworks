9b617d4a1a0436840169bcad836ba2d08a2f768d
DuDolina95 <110253645+DuDolina95@users.noreply.github.com>
2022-08-19 14:10:24 +0300

    Add files via upload
    Домашнее задание №4

--- Program.cs
+++ Program.cs
@@ -1,33 +1,39 @@
 ﻿//Задание 1 Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 
-
-Math.Pow(x, stepen);
+void Exponentiation (int A,int B)
 {
-int x = a;
-int stepen = b;
-Console.WriteLine("a*b = " + Math.Pow(x, stepen));
+        int degree = 1;
+        int result = 1;
+        for (int i = 1; i <= B; i++)
+        {
+            degree = result * A;
+            result = degree ;
+        }
+        Console.WriteLine ($"Число {A} в степени {B} = {result}");
 }
-
-Console.Write("Введите число a: ");
-int x = Convert.Toint32(Console.Readline());
-
-Console.Write("Введите число b: ");
-int stepen = Convert.Toint32(Console.Readline());
+Console.Write ("Введите число A : ");
+int A = Convert.ToInt32 (Console.ReadLine());
+Console.Write ("Введите число B - степень в которую возвести: ");
+int B = Convert.ToInt32 (Console.ReadLine());
+Exponentiation(A,B);
 
 //Задание 2Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 
-int FindSum (int number)
+int FindSumDigits (int number)
 {
-    int Sum = 1
-    for (int current = 1; current <= number; current++)
-{
-    Sum = current + Sum;
+    int ostatok = 0;
+    int sum = 0;
+    while (number > 0)
+    {
+        ostatok = number % 10;
+        sum = sum + ostatok;
+        number = number / 10;
     }
-    return Sum;
+    return sum ;
 }
- Console.Write ("Введитье число");
- int a = Convert.ToInt32 (Console.ReadLine());
- Console.WriteLine ($"Сумма чисел от 1 до {a} = {FindSum (a)}");
+Console.Write ("Введите число: ");
+int number = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine ($"Сумма цифр в числе {number} = {FindSumDigits (number)}");
 
  // Задание 3 Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
  int [] CreateRandomArray(int size)
@@ -42,3 +48,14 @@ int FindSum (int number)
     }
     return newArray;
 }
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    
+    Console.WriteLine();
+}
+Console.Write ("Введите длину массива: ");
+int a = Convert.ToInt32 (Console.ReadLine());
+int[] myArray = CreateRandomArray (a);
+ShowArray(myArray);
